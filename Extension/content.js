ffa04b066d050f568745462f53e9e4f4b178645c
Dobiko <kobidogao@yahoo.com>
2026-04-10 16:41:05 +0200

    Fix instagram videos

--- Extension/content.js
+++ Extension/content.js
@@ -20,6 +20,27 @@ function code() {
   }
 
   console.log('OwO wat dis!');
+  
+  // FIx instagram videos
+  if (window.location.hostname === "www.instagram.com") {
+	document.querySelectorAll('video').forEach(video => {
+	  // Add controls attribute
+	  video.setAttribute('controls', 'true');
+	  // Fix volume
+	  video.volume = parseFloat(localStorage.getItem('videoVolume')) || 0.7;
+	  video.addEventListener('volumechange', () => {
+		localStorage.setItem('videoVolume', video.volume);
+	  });
+
+	  // Remove weird instagram controls
+	  const parent = video.parentElement;
+	  Array.from(parent.children).forEach(child => {
+		if (child !== video) {
+		  parent.removeChild(child);
+		}
+	  });
+	});
+  }
 }
 
 code()
