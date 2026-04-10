27deb77526adb072a886e8526da2ba671e4312a0
Dobiko <kobidogao@yahoo.com>
2026-04-10 17:18:08 +0200

    make volume sync site invariant

--- Extension/content.js
+++ Extension/content.js
@@ -2,12 +2,15 @@ window.onload = PublicStaticMainStringArgs;
 window.onafterupdate = PublicStaticMainStringArgs;
 window.ontransitionend = PublicStaticMainStringArgs;
 window.onclick = PublicStaticMainStringArgs;
+document.addEventListener("click", () => {
+  PublicStaticMainStringArgs();
+});
 
 var executed = false;
 
 function PublicStaticMainStringArgs() {
   RepeatedEvent();
-  
+
   if (!executed) {
     executed = true;
   } else {
@@ -17,32 +20,47 @@ function PublicStaticMainStringArgs() {
   InitEvent();
 }
 
+// ------------------------------------------------
+
+// --- Init ---
+
 function InitEvent() {
-  console.log('OwO wat dis!');
+  console.log("OwO wat dis!");
 }
+
+// --- Repeated ---
 
 function RepeatedEvent() {
+  // Sync chromium webplayer
+  document.querySelectorAll("video").forEach((video) => {
+    // Add controls attribute
+    video.setAttribute("controls", "true");
+    // Fix volume
+    video.volume = parseFloat(localStorage.getItem("videoVolume")) || 0.7;
+    video.removeEventListener("volumechange", () => StoreVideoVolume(video));
+    video.addEventListener("volumechange", () => StoreVideoVolume(video));
+  });
+
   // Fix instagram videos
   if (window.location.hostname === "www.instagram.com") {
-	document.querySelectorAll('video').forEach(video => {
-	  // Add controls attribute
-	  video.setAttribute('controls', 'true');
-	  // Fix volume
-	  video.muted = false;
-	  video.volume = parseFloat(localStorage.getItem('videoVolume')) || 0.7;
-	  video.addEventListener('volumechange', () => {
-		localStorage.setItem('videoVolume', video.volume);
-	  });
+    document.querySelectorAll("video").forEach((video) => {
+      video.muted = false;
 
-	  // Remove weird instagram controls
-	  const parent = video.parentElement;
-	  Array.from(parent.children).forEach(child => {
-		if (child !== video) {
-		  parent.removeChild(child);
-		}
-	  });
-	});
+      // Remove weird instagram controls
+      const parent = video.parentElement;
+      Array.from(parent.children).forEach((child) => {
+        if (child !== video) {
+          parent.removeChild(child);
+        }
+      });
+    });
   }
 }
+
+function StoreVideoVolume(video) {
+  localStorage.setItem("videoVolume", video?.volume);
+}
+
+// ------------------------------------------------
 
 PublicStaticMainStringArgs();
